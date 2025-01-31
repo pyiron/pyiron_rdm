8185537e2f4b3d41ee58e924e547226b67964f89
pkruzikova <p.kruziku@gmail.com>
2025-01-31 12:07:06 +0100

    Actual initial vasp cdict draft

--- ob/concept_dict.py
+++ ob/concept_dict.py
@@ -55,6 +55,18 @@ def process_murnaghan_job(job):
         json.dump(method_dict, f, indent=2)
     return method_dict
 
+def process_vasp_job(job):
+    method_dict = {}
+    add_vasp_contexts(method_dict)
+    identify_vasp_method(job, method_dict)
+    extract_vasp_calculated_quantities(job, method_dict)
+    add_simulation_software(job, method_dict)
+    get_simulation_folder(job, method_dict)
+    file_name = job.path + '_concept_dict.json'
+    with open(file_name, 'w') as f:
+        json.dump(method_dict, f, indent=2)
+    return method_dict
+
 def add_lammps_contexts(method_dict):
     method_dict['@context'] = {}
     method_dict['@context']['sample'] = 'http://purls.helmholtz-metadaten.de/cmso/AtomicScaleSample'
@@ -251,7 +263,7 @@ def extract_lammps_calculated_quantities(job, method_dict):
     """
     aen = np.mean(job.output.energy_tot)
     fen = job.output.energy_tot[-1]
-    fpe = job.output.energy_tot[-1]
+    fpe = job.output.energy_tot[-1] # TODO: are these always supposed to be the same?
     avol = np.mean(job.output.volume)
     fvol = job.output.volume[-1]
     fmax = job.output.force_max[-1]
@@ -826,6 +838,122 @@ def get_structure_folder(structure_path, sample_dict):
         s = str((output.decode('utf-8')))
         s_clean = s.replace("\n", "")
     sample_dict['path'] = s_clean + '/' + structure_path
+
+def add_vasp_contexts(method_dict):
+    # TODO: check, expand on
+    method_dict['@context'] = {}
+    method_dict['@context']['sample'] = 'http://purls.helmholtz-metadaten.de/cmso/AtomicScaleSample'
+    method_dict['@context']['path'] = 'http://purls.helmholtz-metadaten.de/cmso/hasPath'
+    method_dict['@context']['dof'] = 'http://purls.helmholtz-metadaten.de/asmo/hasRelaxationDOF'
+    method_dict['@context']['inputs'] = 'http://purls.helmholtz-metadaten.de/asmo/hasInputParameter'
+    method_dict['@context']['label'] = 'http://www.w3.org/2000/01/rdf-schema#label'
+    method_dict['@context']['unit'] = 'http://purls.helmholtz-metadaten.de/asmo/hasUnit'
+    method_dict['@context']['value'] = 'http://purls.helmholtz-metadaten.de/asmo/hasValue'
+    method_dict['@context']['outputs'] = 'http://purls.helmholtz-metadaten.de/cmso/hasCalculatedProperty'
+    method_dict['@context']['workflow_manager'] = 'http://demo.fiz-karlsruhe.de/matwerk/E457491'
+    #method_dict['@context']['software'] = '"@id": "https://www.vasp.at/","label": "VASP"'
+    method_dict['@context']['dft'] = "http://purls.helmholtz-metadaten.de/asmo/DensityFunctionalTheory"
+    method_dict['@context']['xc_functional'] = 'https://w3id.org/mdo/calculation/hasXCFunctional'
+
+def identify_vasp_method(job, method_dict):
+    # copy-pasted from pyiron-conceptual-dictionary
+    indf = job.input.incar.to_dict()
+    params = indf['data_dict']['Parameter']
+    vals = indf['data_dict']['Value']
+    mlist = []
+    for p,v in zip(params, vals):
+        mlist.append(p + '=' + v)
+    mstring = ';'.join(mlist)
+    raw = mstring.split(';')
+    mdict = {}
+    for r in raw:
+        rsplit = r.split('=')
+        if len(rsplit) == 2:
+            mdict[rsplit[0].replace(' ','')] = rsplit[1].replace(' ','')
+    dof = []
+    if 'ISIF' in mdict.keys():
+        if mdict['ISIF'] in ['0', '1', '2']:
+            dof.append('AtomicPositionRelaxation')
+        elif mdict['ISIF'] == '3':
+            dof.append('AtomicPositionRelaxation')
+            dof.append('CellShapeRelaxation')
+            dof.append('CellVolumeRelaxation')
+        elif mdict['ISIF'] == '4':
+            dof.append('AtomicPositionRelaxation')
+            dof.append('CellShapeRelaxation')
+        elif mdict['ISIF'] == '5':
+            dof.append('CellShapeRelaxation')
+        elif mdict['ISIF'] == '6':
+            dof.append('CellShapeRelaxation')
+            dof.append('CellVolumeRelaxation')
+        elif mdict['ISIF'] == '7':
+            dof.append('CellVolumeRelaxation')
+        elif mdict['ISIF'] == '8':
+            dof.append('AtomicPositionRelaxation')
+            dof.append('CellVolumeRelaxation')
+    if 'NSW' in mdict.keys():
+        if mdict['NSW'] == '0':
+            dof = []
+
+    method_dict['dft'] = {}
+    method_dict['dof'] = dof
+
+    method_dict['dft']['inputs'] = []
+
+    encut_dict = {}
+    encut_dict['value'] = mdict['ENCUT']
+    encut_dict['label'] = 'energy_cutoff'
+    encut_dict['unit'] = 'EV'
+    method_dict['dft']['inputs'].append(encut_dict)
+
+    indf = job.input.to_dict()['kpoints/data_dict']
+    params = indf['Parameter']
+    vals = indf['Value']   
+
+    kpoint_type = vals[2]
+    kpoint_grid = vals[3]
+
+    kpoint_dict = {}
+    kpoint_dict['label'] = f'kpoint_{kpoint_type}'
+    kpoint_dict['value'] = kpoint_grid
+    method_dict['dft']['inputs'].append(kpoint_dict)
+
+    indf = job.input.to_dict()['potcar/data_dict']
+    xc = indf['Value'][0]
+    method_dict['xc_functional'] = xc
+
+def extract_vasp_calculated_quantities(job, method_dict):
+    # mostly copy-pasted from pyiron_concept_dictionary // lammps
+    # TODO how many decimals? lammps does 4, not 5
+    """
+    Extracts calculated quantities from a job.
+
+    Parameters
+    ----------
+    job : pyiron.Job
+        The job object containing the calculated quantities.
+
+    Returns
+    -------
+    list
+        A list of dictionaries, each containing the label, value, unit, and associate_to_sample of a calculated quantity.
+
+    """
+    outputs = []
+    outputs.append(
+        {
+            "label": "average_total_energy",
+            "value": np.round(job.output.energy_tot[-1], decimals=5),
+            "unit": "EV",
+        }
+    )
+    outputs.append(
+        {
+            "label": "final_total_volume",
+            "value": np.round(job.output.volume[-1], decimals=5),
+            "unit": "ANGSTROM3",
+        }
+    )
 
 def export_env(path):
     """Exports to path+_environment.yml"""
