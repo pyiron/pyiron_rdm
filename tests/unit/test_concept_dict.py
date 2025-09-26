448007959380959b628102d45ee5a40e057c4682
Sam Waseda <waseda@mpie.de>
2025-09-26 09:47:31 +0200

    Add more elements

--- tests/unit/test_concept_dict.py
+++ tests/unit/test_concept_dict.py
@@ -7,9 +7,9 @@ from pyiron_rdm.concept_dict import get_unit_cell_parameters
 
 class TestVersion(unittest.TestCase):
     def test_get_unit_cell_parameters(self):
-        structure = bulk("Al", cubic=True)
+        Al = bulk("Al", cubic=True)
         self.assertEqual(
-            get_unit_cell_parameters(structure),
+            get_unit_cell_parameters(Al),
             {
                 "a": 4.05,
                 "alpha": 90.0,
@@ -20,6 +20,25 @@ class TestVersion(unittest.TestCase):
                 "space_group_number": 225,
                 "bravais_lattice": "fcc",
             },
+        )
+        Fe = bulk("Fe")
+        self.assertEqual(
+            get_unit_cell_parameters(Fe),
+            {
+                "a": 2.87,
+                "alpha": 90.0,
+                "beta": 90.0,
+                "gamma": 90.0,
+                "volume": 23.6399,
+                "space_group": "Im-3m",
+                "space_group_number": 229,
+                "bravais_lattice": "bcc",
+            },
+        )
+        Mg = bulk("Mg")
+        self.assertEqual(
+            get_unit_cell_parameters(Mg)["bravais_lattice"],
+            "hcp",
         )
 
 
