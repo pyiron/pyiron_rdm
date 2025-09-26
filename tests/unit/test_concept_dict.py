f3e07d1d731721e5bf6bce9051c340d07ecaa38e
Sam Waseda <waseda@mpie.de>
2025-09-26 08:58:16 +0200

    Replace get_symmetry from pyiron_atomistics by structuretoolkit

---
+++ tests/unit/test_concept_dict.py
@@ -0,0 +1,26 @@
+import unittest
+
+from ase.build import bulk
+from pyiron_rdm.concept_dict import get_unit_cell_parameters
+
+
+class TestVersion(unittest.TestCase):
+    def test_get_unit_cell_parameters(self):
+        structure = bulk("Al", cubic=True)
+        self.assertEqual(
+            get_unit_cell_parameters(structure),
+            {
+                "a": 4.05,
+                "alpha": 90.0,
+                "beta": 90.0,
+                "gamma": 90.0,
+                "volume": 66.4301,
+                "space_group": "Fm-3m",
+                "space_group_number": 225,
+                "bravais_lattice": "fcc",
+            },
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
