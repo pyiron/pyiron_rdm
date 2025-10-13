94a53f4c2e7e9232ba855cf0cb486701575775c8
Sam Waseda <waseda@mpie.de>
2025-10-13 12:07:03 +0200

    Make a full comparison

--- tests/unit/test_ob_cfg.py
+++ tests/unit/test_ob_cfg.py
@@ -11,19 +11,22 @@ class TestObCFG(unittest.TestCase):
         d = os.path.dirname(os.path.realpath(__file__))
         with open(os.path.join(d, "..", "static", "lammps_concept_dict.json")) as f:
             concept_dicts = json.load(f)
+        props_dict = {}
         for key, concept_dict in concept_dicts.items():
             cdict = flatten_cdict(concept_dict)
-            props_dict = ob_cfg_sfb1394.map_cdict_to_ob(
+            p_dict = ob_cfg_sfb1394.map_cdict_to_ob(
                 user_name="test",
                 cdict=cdict,
                 concept_dict=concept_dict,
             )
-            if key == "structure":
-                self.assertEqual(props_dict["$name"], "Fe_minimize_structure")
-            if key == "job":
-                self.assertEqual(props_dict["$name"], "Fe_minimize")
-            if key == "final_structure":
-                self.assertEqual(props_dict["$name"], "Fe_minimize_final_structure")
+            p_dict.pop("pyiron_conceptual_dictionary")
+            p_dict.pop("description_multiline")
+            if "date" in p_dict:
+                p_dict.pop("date")
+            props_dict[key] = p_dict
+        with open(os.path.join(d, "..", "static", "lammps_props_dict_sfb.json")) as f:
+            props_dict_ref = json.load(f)
+        self.assertDictEqual(props_dict, props_dict_ref)
 
 
 if __name__ == "__main__":
