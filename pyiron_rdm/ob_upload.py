8f785ddd4735d7576e8f32cc4bc89ad52c747ffb
Sam Waseda <waseda@mpie.de>
2025-10-13 10:52:48 +0200

    black

--- pyiron_rdm/ob_upload.py
+++ pyiron_rdm/ob_upload.py
@@ -72,9 +72,7 @@ def openbis_validate(
         cdict = flatten_cdict(concept_dict)
         from importlib import import_module
 
-        object_type, ds_types, inv_parents = import_module(o.ot).get_ot_info(
-            cdict
-        )
+        object_type, ds_types, inv_parents = import_module(o.ot).get_ot_info(cdict)
         map_cdict_to_ob = import_module(o.mapping).map_cdict_to_ob
         props_dict = map_cdict_to_ob(
             user_name=o.get_session_info().userName,
