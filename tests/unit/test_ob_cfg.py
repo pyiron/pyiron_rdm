b4ff1bfb9823a474d11acb40a84f5b412a3450c6
Sam Waseda <waseda@mpie.de>
2025-10-13 11:49:42 +0200

    Add some tests

--- tests/unit/test_ob_cfg.py
+++ tests/unit/test_ob_cfg.py
@@ -9,15 +9,19 @@ class TestObCFG(unittest.TestCase):
     def test_map_cdict_to_ob(self):
         with open("../static/lammps_concept_dict.json") as f:
             concept_dicts = json.load(f)
-        for concept_dict in concept_dicts.values():
+        for key, concept_dict in concept_dicts.items():
             cdict = flatten_cdict(concept_dict)
             props_dict = ob_cfg_sfb1394.map_cdict_to_ob(
                 user_name="test",
                 cdict=cdict,
                 concept_dict=concept_dict,
             )
-            print(props_dict)
-            print("\n\n\n============================\n\n\n")
+            if key == "structure":
+                self.assertEqual(props_dict["$name"], "Fe_minimize_structure")
+            if key == "job":
+                self.assertEqual(props_dict["$name"], "Fe_minimize")
+            if key == "final_structure":
+                self.assertEqual(props_dict["$name"], "Fe_minimize_final_structure")
 
 
 if __name__ == "__main__":
