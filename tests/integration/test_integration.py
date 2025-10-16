8972af6f78cc038cc422af6ab9d269205d17d088
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-10-16 16:54:55 +0200

    run black

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,8 +4,8 @@ import unittest
 
 class TestConf(unittest.TestCase):
     def test_env_present(self):
-        self.assertEqual(os.env[pyironautouser], 'some')
+        self.assertEqual(os.env[pyironautouser], "some")
 
     def test_conf_present(self):
         print(os.getcwd())
-        self.assertTrue(os.path.isfile('some.conf'))
+        self.assertTrue(os.path.isfile("some.conf"))
