75a85c89a26d386be1a033e3d690b456b4735394
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-10-16 16:39:05 +0200

    test env secret var

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -3,6 +3,9 @@ import unittest
 
 
 class TestConf(unittest.TestCase):
+    def test_env_present(self):
+        self.assertEqual(os.env[pyironautouser]), 'some')
+
     def test_conf_present(self):
         print(os.getcwd())
-        self.assertTrue(os.isfile('some.conf'))
+        self.assertTrue(os.path.isfile('some.conf'))
