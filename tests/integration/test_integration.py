661d8019fc787ba8ed6f4d60c32838593e95b16c
Niklas Siemer <70580458+niklassiemer@users.noreply.github.com>
2025-10-16 16:41:49 +0200

    fix syntax

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -4,7 +4,7 @@ import unittest
 
 class TestConf(unittest.TestCase):
     def test_env_present(self):
-        self.assertEqual(os.env[pyironautouser]), 'some')
+        self.assertEqual(os.env[pyironautouser], 'some')
 
     def test_conf_present(self):
         print(os.getcwd())
