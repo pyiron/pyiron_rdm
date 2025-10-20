b0c1c3eeb4f862c6fb57a4fad634b9b3f50ac322
Sam Waseda <waseda@mpie.de>
2025-10-20 08:11:31 +0200

    Add assignment tools

---
+++ pyiron_rdm/tools.py
@@ -0,0 +1,10 @@
+def get_dataset_assignments(o)
+    return {i.code: i.get_property_assignments().df for i in o.get_dataset_types()}
+
+
+def get_object_assignments(o):
+    return {i.code: i.get_property_assignments().df for i in o.get_object_types()}
+
+
+def get_controlled_vocabulary(o):
+    return {i.code: i.get_terms().df for i in o.get_vocabularies()}
